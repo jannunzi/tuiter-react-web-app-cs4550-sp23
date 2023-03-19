d6751890e82dca1a1d4163846150114db7262332
Jose Annunziato <jose.annunziato@sonos.com>
2023-03-19 15:45:07 -0400

    hash router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import SimpleComponent from "./SimpleComponent";
 import Loops from "./Loops";
 import Add from "./Add";
 import Destructors from "./Destructors";
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter, HashRouter, Link } from "react-router-dom";
 import { Routes, Route } from "react-router";
 import Home from "./Home";
 import Tarp from "./tarp";
@@ -16,7 +16,7 @@ function App() {
   return (
     <div>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <Link to="/">Home</Link> | <Link to="/add">Add</Link> |
           <Link to="/simple">Simple Component</Link> |
           <Link to="/destructor">Destructor</Link>|<Link to="loops">Loops</Link>
@@ -32,7 +32,7 @@ function App() {
             <Route path="/tarp" element={<Tarp />} />
             <Route path="/todos" element={<TodoList />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     </div>
   );
